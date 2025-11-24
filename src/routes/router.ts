5beaa3a766be5a1d4c92adc3ce37cd9b9860d616
yangjinhong8888 <yangjinhong8888@gmail.com>
2025-11-24 21:08:22 +0800

    feat: 更新注释

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -9,7 +9,7 @@ const router = createBrowserRouter([
     path: "/",
     Component: AdminLayout,
     id: "root",
-    // 用于验证当前是否登录，如未登录则直接跳转到login页面
+    // 用于验证当前是否登录，如未登录则直接跳转到login页面，放在需要访问的根拦截
     loader: userInfoLoader,
     children: [
       {
